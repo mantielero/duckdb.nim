1df288e660deb2797ae808c891757ec3da8e54b7
Jose Maria Garcia <josemaria.alkala@gmail.com>
2024-12-06 12:06:26 +0100

    unsigned integers case

--- examples/ex20_chunk_integers.nim
+++ examples/ex20_chunk_integers.nim
@@ -22,24 +22,18 @@ for i in 0 .. r.colNames.high:
 
 
 
-# Integer
-# conn.sql("CREATE TABLE integers (i INTEGER, j INTEGER);")
-# conn.sql("INSERT INTO integers VALUES (3, 4), (5, 6), (7, NULL), (8,9), (100,11);")
-# var res = conn.sql("SELECT * FROM integers;")
+# Unsigned Integers
+conn.sql("CREATE TABLE unsignedintegers (ti UTINYINT, si USMALLINT, i UINTEGER, bi UBIGINT);")
+conn.sql("INSERT INTO unsignedintegers VALUES (0, 0, 0, 0), (NULL, NULL, NULL, NULL), (255, 65535, 4294967295, 18446744073709551615), (1,2,3, 4);")
+res = conn.sql("SELECT * FROM unsignedintegers;")
 
-# var r = res.toDbResult()
-# echo "Number of rows: ", r.rows
+r = res.toDbResult()
+echo "\n\n\nNumber of rows: ", r.rows
 
-# echo "\nColumn:  name: ", r.colNames[0], "  type: ", r.colTypes[0]
-# for i in r.getInts(0):
-#   if i.isSome:
-#     echo i.get
-#   else:
-#     echo "NULL"
-
-# echo "\nColumn:  name: ", r.colNames[1], "  type: ", r.colTypes[1]
-# for i in r.getInts(1):
-#   if i.isSome:
-#     echo i.get
-#   else:
-#     echo "NULL"
+for i in 0 .. r.colNames.high:
+  echo "\nColumn:  name: ", r.colNames[i], "  type: ", r.colTypes[i]
+  for i in r.getUints(i):
+    if i.isSome:
+      echo i.get
+    else:
+      echo "NULL"
